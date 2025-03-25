4c9386011a72edba509928756544ff3f0593e871
Yevhenii Prasol <mefgner@gmail.com>
2025-03-25 12:24:27 +0100

    Add popup notification system with global error handling

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 <template>
   <div class="flex min-h-[100vh] flex-col dark:bg-gray-900 dark:text-gray-300">
+    <PopupView />
     <router-view />
   </div>
 </template>
+<script setup lang="ts">
+import PopupView from '@/components/PopupView.vue'
+</script>
