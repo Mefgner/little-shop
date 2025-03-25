422dfa1e8b5560402f49da82862cd78527d3f453
Yevhenii Prasol <mefgner@gmail.com>
2025-03-25 13:35:27 +0100

    move modalbase

--- src/components/modal/ModalBase.vue
+++ src/components/modal/ModalBase.vue
@@ -11,10 +11,10 @@ const props = defineProps({
 
 <template>
   <div>
-    <div class="fixed inset-0 z-30 bg-gray-900 opacity-50 dark:opacity-75"></div>
-    <div class="fixed inset-5 z-50 md:py-4">
+    <div class="fixed inset-0 z-10 bg-gray-900 opacity-50 dark:opacity-75"></div>
+    <div class="fixed inset-5 z-20 md:py-4">
       <div
-        class="mx-auto flex h-full max-h-full flex-col rounded-2xl bg-white shadow-lg md:w-[660px] dark:bg-gray-900"
+        class="mx-auto flex h-full max-h-full w-full flex-col rounded-2xl bg-white shadow-lg md:w-fit dark:bg-gray-900"
       >
         <div
           class="flex min-h-16 items-center justify-between border-b-2 border-gray-300 px-8 dark:border-gray-700"
