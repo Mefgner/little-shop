ac98923daa6506dcbd8d8041cc457a933da58dd1
Yevhenii Prasol <mefgner@gmail.com>
2025-03-25 13:47:40 +0100

    Update ModalCart component layout and messaging for better user experience

--- src/components/modal/cart/ModalCart.vue
+++ src/components/modal/cart/ModalCart.vue
@@ -15,7 +15,10 @@ const props = defineProps({
 </script>
 
 <template>
-  <div v-if="usedCart.cart.length > 0" class="flex flex-col overflow-y-auto px-2 pb-8 md:px-8">
+  <div
+    v-if="usedCart.cart.length > 0"
+    class="flex w-[660px] flex-col overflow-y-auto px-2 pb-8 md:px-8"
+  >
     <div v-if="usedCart.batchedCart.length > 0">
       <CartItem
         :count="count"
@@ -28,7 +31,7 @@ const props = defineProps({
       <PrimaryBtn v-if="props.doShowPurchase"> Checkout</PrimaryBtn>
     </button>
   </div>
-  <div v-else class="flex h-[40vh] flex-col items-center justify-center">
-    <p class="mt-[-10px] text-xl">No items in useCart</p>
+  <div v-else class="flex h-full w-[660px] flex-col items-center justify-center">
+    <p class="mt-[-10px] text-xl">No items in cart</p>
   </div>
 </template>
