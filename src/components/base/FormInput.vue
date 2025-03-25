601d60f4e9031d37f8ef111ee32250e4b13159db
Yevhenii Prasol <mefgner@gmail.com>
2025-03-25 16:37:04 +0100

    Remove .editorconfig, add PrettyInput component, and update UserRegister form inputs

---
+++ src/components/base/FormInput.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const val = ref('')
+</script>
+
+<template>
+  <input
+    v-model="val"
+    @change="(value) => $emit('modelValue', val)"
+    class="w-full rounded border border-gray-300 p-2"
+  />
+</template>
--- src/components/modal/user/parts/UserRegister.vue
+++ src/components/modal/user/parts/UserRegister.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import FormInput from '@/components/base/FormInput.vue'
+import PrettyInput from '@/components/base/PrettyInput.vue'
 import PrimaryBtn from '@/components/base/PrimaryBtn.vue'
 import { ref } from 'vue'
 import type { User } from '@/store/auth.ts'
@@ -10,20 +10,20 @@ const data = ref<User>({} as User)
 <template>
   <div class="flex w-[660px] flex-col items-center gap-4">
     <form class="flex max-w-72 flex-col items-center gap-3">
-      <FormInput v-model="data.name" type="text" placeholder="Full Name" required />
-      <FormInput v-model="data.email" type="email" placeholder="Email" required />
-      <FormInput v-model="data.password" type="password" placeholder="Password" required />
+      <PrettyInput v-model="data.name" type="text" placeholder="Full Name" required />
+      <PrettyInput v-model="data.email" type="email" placeholder="Email" required />
+      <PrettyInput v-model="data.password" type="password" placeholder="Password" required />
 
       <p class="mt-2 w-full">Not required fields down</p>
-      <FormInput v-model="data.phone" type="tel" placeholder="Phone Number" />
+      <PrettyInput v-model="data.phone" type="tel" placeholder="Phone Number" />
 
       <div class="flex gap-2">
-        <FormInput v-model="data.deliveryAddress" type="text" placeholder="Delevery Address" />
-        <FormInput v-model="data.deliveryZipCode" type="text" placeholder="Postal Code" />
+        <PrettyInput v-model="data.deliveryAddress" type="text" placeholder="Delevery Address" />
+        <PrettyInput v-model="data.deliveryZipCode" type="text" placeholder="Postal Code" />
       </div>
       <div class="flex gap-2">
-        <FormInput v-model="data.deliveryCity" type="text" placeholder="City" />
-        <FormInput v-model="data.deliveryCountry" type="text" placeholder="Country" />
+        <PrettyInput v-model="data.deliveryCity" type="text" placeholder="City" />
+        <PrettyInput v-model="data.deliveryCountry" type="text" placeholder="Country" />
       </div>
       <button class="mt-4" @click.prevent="$emit('register', data)">
         <PrimaryBtn class="w-fit"> Register </PrimaryBtn>
